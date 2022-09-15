6a49b6023a0921383f6a5a921cda2e38d33029f2
uxiun <teppeiito0905@gmail.com>
2022-09-15 12:32:14 +0900

    c

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : undefined
-const path = require("path");
+// const path = require("path");
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
